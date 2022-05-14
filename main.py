89e396f8357d57f6a6bdeba85a89a3f4f1b852d5
František Bráblík <frantisek.brablik@gmail.com>
2022-05-14 14:43:03 +0200

    It's solving

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 
 
 from sys import stderr
-import time
 from typing import List, Optional
 from enum import Enum
+import subprocess
+import time
 
 
 from selenium import webdriver
@@ -14,13 +15,19 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 
-class Tile(Enum):
+class Clue(Enum):
     GRAY   = '⬛'
     YELLOW = '🟨'
     GREEN  = '🟩'
 
 
-def send_word(driver, body, word: str) -> Optional[List[Tile]]:
+class Tile:
+    def __init__(self, char: str, clue: Clue):
+        self.char = char
+        self.clue = clue
+
+
+def send_word(driver, body, idx: int, word: str) -> Optional[List[Tile]]:
     body.send_keys(word + "\n")
 
     driver.implicitly_wait(2.5)
@@ -29,7 +36,7 @@ def send_word(driver, body, word: str) -> Optional[List[Tile]]:
 
     tiles = driver.find_element(By.XPATH, '/html/body/game-app')  \
                   .shadow_root  \
-                  .find_element(By.CSS_SELECTOR, 'div div div game-row')  \
+                  .find_elements(By.CSS_SELECTOR, 'game-theme-manager div div div game-row')[idx]  \
                   .shadow_root  \
                   .find_element(By.CSS_SELECTOR, 'div')  \
                   .find_elements(By.CSS_SELECTOR, 'game-tile')
@@ -43,11 +50,11 @@ def send_word(driver, body, word: str) -> Optional[List[Tile]]:
         if value is None:
             return None
         elif value == 'absent':
-            res.append(Tile.GRAY)
+            res.append(Tile(letter, Clue.GRAY))
         elif value == 'present':
-            res.append(Tile.YELLOW)
+            res.append(Tile(letter, Clue.YELLOW))
         elif value == 'correct':
-            res.append(Tile.GREEN)
+            res.append(Tile(letter, Clue.GREEN))
         else:
             print("invalid value", file=stderr)
             return None
@@ -67,10 +74,35 @@ def send_word(driver, body, word: str) -> Optional[List[Tile]]:
     return res
 
 
-def show(result: Optional[List[Tile]]) -> None:
+def show(result: List[Tile]) -> None:
     for el in result:
-        print(el.value, end='')
+        print(el.clue.value, end='')
     print()
+
+
+def all_correct(result: List[Tile]) -> bool:
+    return all(map(lambda x: x.clue == Clue.GREEN, result))
+
+
+def next_guess(tiles: List[List[Tile]],
+               banned: Optional[List[str]] = None) -> str:
+    if banned is None:
+        banned = []
+
+    arg: str = ''
+    for i in range(5):
+        for tile in tiles:
+            if tile[i].clue == Clue.YELLOW:
+                arg += '^'
+            elif tile[i].clue == Clue.GRAY:
+                arg += '!'
+            arg += tile[i].char
+        arg += '.'
+
+    solve = subprocess.Popen(['./solver.hs', arg], stdout=subprocess.PIPE)
+    word =  solve.stdout.readline().decode('utf-8').replace('\n', '')
+    solve.kill()
+    return word
 
 
 service = Service(executable_path="./chromedriver")
@@ -92,14 +124,31 @@ body = driver.find_element(By.TAG_NAME, "body")
 # close help overlay
 body.click()
 
-a = send_word(driver, body, "aeons")
-show(a)
 
-b = send_word(driver, body, "weary")
-show(b)
+tiles: List[List[Tile]] = []
+i: int = 0
+while i < 5:
 
+    word: str = next_guess(tiles)
+    ret = send_word(driver, body, i, word)
+
+    if ret is None:
+        i -= 1
+    else:
+        tiles.append(ret)
+
+    show(ret)
+
+    if all_correct(ret):
+        break
+
+    i += 1
+
+
+driver.save_screenshot('screen.png')
 
 time.sleep(2.4)
+
 
 driver.quit()
 
